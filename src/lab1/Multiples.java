187a710a4cf280d4f29c3356ea8f9fb7dc4421d2
navdeep <navdeepsg04@gmail.com>
2025-09-08 16:48:07 -0400

    fixed bug

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -11,7 +11,7 @@ public class Multiples {
 
         int nums = 0;
 
-        for (int i = 1; i <= range; i++) {
+        for (int i = 1; i < range; i++) {
 
             boolean divisibleByA = i % a == 0;
             boolean divisibleByB = i % b == 0;
